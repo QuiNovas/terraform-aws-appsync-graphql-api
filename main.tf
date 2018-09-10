532b899db3b4c2b1c9c186bb336d643c4ba443a0
Joseph Wortmann <joseph.wortmann@gmail.com>
2018-09-10 09:26:58 -0500

    Initial checkin

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "aws_appsync_graphql_api" "graphql_api" {
+  authentication_type = "${var.name}"
+  name                = "${var.name}"
+  user_pool_config    = "${var.user_pool_config}"
+}
+
+data "external" "graphql_endpoint" {
+  program = [
+    "bash",
+    "${path.module}/get-graphql-endpoint.sh"
+  ]
+  query {
+    api_id = "${aws_appsync_graphql_api.graphql_api.id}"
+  }
+}
